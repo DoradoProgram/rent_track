919c127fd98a5c6cd5efd2ca4a69712c0f228075
DoradoProgram <163253982+DoradoProgram@users.noreply.github.com>
2026-05-21 10:21:49 +0000

    Firebase key complete

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "xxx",
-  authDomain: "xxx",
-  projectId: "xxx",
-  storageBucket: "xxx",
-  messagingSenderId: "xxx",
-  appId: "xxx"
+  apiKey: "AIzaSyCjc8PC_oJBWtECf4f89D6jYrNjpxDb-Ls",
+  authDomain: "rent-track-e8e32.firebaseapp.com",
+  projectId: "rent-track-e8e32",
+  storageBucket: "rent-track-e8e32.firebasestorage.app",
+  messagingSenderId: "381379480202",
+  appId: "1:381379480202:web:904586eec4cb28004b7b73"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
